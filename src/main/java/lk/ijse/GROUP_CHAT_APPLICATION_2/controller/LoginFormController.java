45fcf84d3eb8652f50c7924696359937261c09a7
Sachintha-Sandaruwan-Bandara <sachib843@gmail.com>
2024-01-17 17:03:33 +0530

    content update

--- src/main/java/lk/ijse/GROUP_CHAT_APPLICATION_2/controller/LoginFormController.java
+++ src/main/java/lk/ijse/GROUP_CHAT_APPLICATION_2/controller/LoginFormController.java
@@ -3,6 +3,16 @@ package lk.ijse.GROUP_CHAT_APPLICATION_2.controller;
     @author Sachi_S_Bandara
     @created 1/17/2024 - 4:53 PM 
 */
-
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
 public class LoginFormController {
+    @FXML
+    private TextField txtUserName;
+
+    @FXML
+    void btnLoginOnAction(ActionEvent event) {
+
+    }
+
 }
